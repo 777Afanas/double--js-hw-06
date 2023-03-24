00bc65022a10269b632d3034f3faa038c4582899
777Afanas <serhii2111@yahoo.com>
2023-03-24 15:08:38 +0200

    Task 01 end

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,21 +1,18 @@
 
-const list = document.querySelector('#categories');
-console.log(list.childNodes);
-// console.log(list.firstElementChild);
-// console.log(list.lastElementChild);
-// console.log(list.children);
+const list = document.querySelector('#categories');  
 const numberCategories = list.children.length;
 console.log(`Number of categories: ${numberCategories}`);
-// const listAll = document.querySelectorAll(".item");
-// console.log(listAll);
 
-const textAll = document.querySelectorAll("h2");
-console.log(textAll);
-textAll.forEach(function (text, index, array) {
-       
-    console.log(`Category: ${text.textContent}`);
-
+const itemELs = document.querySelectorAll(".item");  
+itemELs.forEach(function (itemEL) {   
+    console.log(`Category: ${itemEL.firstElementChild.textContent}`);
+    console.log(`Elements: ${itemEL.lastElementChild.children.length}`);   
 });
-// childNodes
 
-const ttsAll = textAll.childNodes;
+// itemELs.forEach(function (itemEL, index) {     
+//     console.log(`Category: ${itemELs[index].firstElementChild.textContent}`);
+//     console.log(`Elements: ${itemELs[index].lastElementChild.children.length}`);         
+// });
+
+
+
