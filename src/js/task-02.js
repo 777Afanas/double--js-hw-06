1d705ec24d409cc0db1b13ce3171be965a0f7ea5
777Afanas <serhii2111@yahoo.com>
2023-03-24 17:29:32 +0200

    Task 02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -5,4 +5,52 @@ const ingredients = [
   'Tomatos',
   'Herbs',
   'Condiments',
-];
+];   
+
+const ingredientsContainerEl = document.querySelector('#ingredients'); 
+
+//================OLD_SCHOOL==============================
+
+// const elements = [];
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const ingredient = ingredients[i];
+
+//   const ingredientEl = document.createElement("li");
+//   ingredientEl.textContent = ingredient;
+//   ingredientEl.classList.add("item");
+
+//   elements.push(ingredientEl);
+// }
+// ingredientsContainerEl.append(...elements);
+// console.log(ingredientsContainerEl);
+
+//===================================================
+
+// const elements = ingredients.map(ingredient => {
+//   const ingredientEl = document.createElement("li");
+//   ingredientEl.textContent = ingredient;
+//   ingredientEl.classList.add("item");
+
+//   return ingredientEl;
+// });
+
+// ingredientsContainerEl.append(...elements);
+// console.log(ingredientsContainerEl);
+
+//=================FUNCTION===============================
+
+// const makeComponents = components => { 
+const makeComponents = ingredients => { 
+
+  return ingredients.map(ingredient => {
+    const ingredientEl = document.createElement("li");
+    ingredientEl.textContent = ingredient;
+    ingredientEl.classList.add("item");
+
+    return ingredientEl;
+  });
+};
+
+const elements = makeComponents(ingredients);
+ingredientsContainerEl.append(...elements);
+console.log(ingredientsContainerEl);
