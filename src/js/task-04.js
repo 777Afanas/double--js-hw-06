68b218d22fef74965048795c8e022ed60b066ef9
777Afanas <serhii2111@yahoo.com>
2023-04-05 23:15:42 +0300

    task 04 and

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,6 +1,6 @@
 const decrementBtn = document.querySelector('[data-action="decrement"]');
 const incrementBtn = document.querySelector('[data-action="increment"]');
-const counter = document.querySelector("#value");
+const counterEl = document.querySelector("#value");
 
 let counterValue = 0;
 
@@ -10,12 +10,29 @@ incrementBtn.addEventListener("click", onIncrClick);
 function onDecrClick (event) {
     event.preventDefault();     
     counterValue -= 1;       
-    counter.textContent = counterValue; 
+    counterEl.textContent = counterValue;        
 };
 
 function onIncrClick (event) {
     event.preventDefault();     
     counterValue += 1;  
-    counter.textContent = counterValue;     
+    counterEl.textContent = counterValue;     
 };
 
+// =================================================
+
+
+// const onDecrClick = () => {
+//     event.preventDefault();     
+//     counterValue -= 1;       
+//     counterEl.textContent = counterValue;   
+// };
+
+// const onIncrClick = () => {
+//     event.preventDefault();
+//     counterValue += 1;
+//     counterEl.textContent = counterValue;
+// };
+
+// decrementBtn.addEventListener("click", onDecrClick);
+// incrementBtn.addEventListener("click", onIncrClick); 
